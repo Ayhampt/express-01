c013fc3d5d988da7704306a1d1c4a7898757e7f4
Ayhampt <ayhampt011@gmail.com>
2025-05-06 07:31:51 +0530

    install winston and morgan packages for request Log ,create app.log

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 import "dotenv/config";
 import express from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
 const port = process.env.PORT || 3000;
 const hostname = "127.0.0.1";
 
 app.use(express.json());
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let cafeMenu = [];
 let nextid = 1;
 //add new product
 app.post("/products", (req, res) => {
+  logger.info("POST request is made to add new item to cafeMenu")
   const { name, price } = req.body;
   const newItem = { id: nextid++, name, price };
   cafeMenu.push(newItem);
